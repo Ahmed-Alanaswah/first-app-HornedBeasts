facce33690dd81c4ee95067301ae343525029c4c
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-09-07 23:20:15 +0300

    solve gallery

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SelectedBeast from "./components/SelectedBeast";
 
- class App extends Component {
-  
-  render() {
-    return (
-      <>
-        <Header/>
-        <Main/>
-        <Footer/>
-      </>
-    )
-  }
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showModal: false,
+			title: "",
+			description: "",
+			image_url: "",
+			raislikes: 0,
+		};
+	}
+
+	increaseLike = () => {
+		this.setState({
+			raislikes: this.state.raislikes + 1,
+		});
+	};
+	handleClose = () => {
+		this.setState({
+			showModal: false,
+		});
+	};
+
+	handleOpen = (title, description, image_url, raislikes) => {
+		this.setState({
+			showModal: true,
+			title: title,
+			description: description,
+			image_url: image_url,
+			raislikes: raislikes,
+		});
+	};
+	render() {
+		return (
+			<>
+				<Header />
+
+				<Main
+					handleOpen={this.handleOpen}
+					raislikes={this.state.raislikes}
+					increaseLike={this.increaseLike}
+				/>
+				<SelectedBeast
+					handleClose={this.handleClose}
+					showModal={this.state.showModal}
+					title={this.state.title}
+					description={this.state.description}
+					image_url={this.state.image_url}
+					raislikes={this.state.raislikes}
+				/>
+				<Footer />
+			</>
+		);
+	}
 }
 
-export default App
+export default App;
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Card, Button, Col } from "react-bootstrap";
 
- class HornedBeasts extends Component {
-    constructor(props){
-        super(props);
-        this.state ={raislikes:0};
-      }
-    increaseLike =()=> {
-        this.setState({
-            raislikes : this.state.raislikes +1
-           })
-    }
+class HornedBeasts extends Component {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = { raislikes: 0 };
+	// }
+	// increaseLike = () => {
+	// 	this.setState({
+	// 		raislikes: this.state.raislikes + 1,
+	// 	});
+	// };
 
-    render() {
-        return (
-            <div>
-                <h2>{this.props.title}</h2>
-                <h2>{this.props.likes}</h2>
-                <h2>{this.state.raislikes}</h2>
-                <img onClick={this.increaseLike} src={this.props.image_url} alt= {this.props.keyword} />
-                <p>{this.props.description}</p>
-    
-            </div>
-        )
-    }
+	getHandleOpen = () => {
+		let title = this.props.title;
+		let description = this.props.description;
+		let raislikes = this.props.raislikes;
+		let image_url = this.props.image_url;
+		this.props.handleOpen(title, description, image_url, raislikes);
+	};
+
+	render() {
+		return (
+			<>
+				<Col>
+					<Card
+						className="text-center"
+						style={{ width: "18rem", margin: "10px auto" }}
+					>
+						<Card.Img
+							variant="top"
+							style={{ height: "200px", objectFit: "cover" }}
+							onClick={this.props.increaseLike}
+							src={this.props.image_url}
+							alt={this.props.keyword}
+						/>
+						<Card.Body
+							style={{
+								height: "235px",
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "space-around",
+							}}
+						>
+							<Card.Title>{this.props.title}</Card.Title>
+							<Card.Text>
+								{this.props.description}
+								<br />
+								<i className="bi bi-suit-heart-fill"></i>:{this.props.raislikes}
+							</Card.Text>
+							<Button onClick={this.getHandleOpen} variant="primary">
+								Go somewhere
+							</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+			</>
+		);
+	}
 }
 
-export default HornedBeasts
+export default HornedBeasts;
