0a984c0d280e1051d5966b9ee23b6dc9c66e0935
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-09-09 13:22:36 +0300

    fix err

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Footer extends Component {
-    render() {
-        return (
-            <div  style={{ backgroundColor:'#000',color:'#fff',textAlign:'center',padding:'15px' }}>
-                <p>all reversed by ahmed alanasweh</p>
-            </div>
-        )
-    }
+class Footer extends Component {
+	render() {
+		return (
+			<div
+				style={{
+					backgroundColor: "#000",
+					color: "#fff",
+					textAlign: "center",
+					padding: "15px",
+				}}
+			>
+				<p>all reversed by ahmed alanasweh &copy;2021</p>
+			</div>
+		);
+	}
 }
 
-export default Footer
+export default Footer;
